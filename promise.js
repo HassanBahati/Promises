0c7a03c50f634d801d92ebecadff6b440f67acf2
HassanBahati <mukisabahati@gmail.com>
2021-03-24 15:58:34 +0300

    edited

--- promise.js
+++ promise.js
@@ -1,10 +1,14 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(()=>{
-        console.log('ggghd')
-        resolve({user: 'bahati'})
+        console.log('got the user')
+        // resolve({user: 'bahati'})
+        reject(new Error('user not logged in '))
     },2000)
 });
 
+//consuming the promise
 promise.then(user => {
     console.log(user)
-})
+}).catch(err =>{
+    console.log(err.message)
+});
