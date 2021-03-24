47e4a8e719880941201a1a5d51178c120b58b5e2
HassanBahati <mukisabahati@gmail.com>
2021-03-24 10:48:44 +0300

    sync code example

---
+++ app.js
@@ -0,0 +1,16 @@
+//sync code example
+//execution in a specified format
+
+function otherFunction(){
+    console.log('We are in another function');
+    console.log('do some stuff')
+}
+
+console.log('start');
+
+otherFunction();
+
+console.log('End');
+
+
+//
