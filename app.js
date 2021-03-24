00ef2bbc6d21f3b4aaa594dc0ddd03f8dfb1db10
HassanBahati <mukisabahati@gmail.com>
2021-03-24 15:36:56 +0300

    callback hell

--- app.js
+++ app.js
@@ -3,12 +3,34 @@ console.log('Start');
 function loginUser(email, password, callback){
     setTimeout(()=>{
         console.log('Now we have the data')
-        return {userEmail: email}
+        callback({userEmail: email})
     }, 5000);
 }
 
-const user = loginUser('devs@mail.com', 123456);
+function getUserVideo(email,callback){
+    setTimeout(() => {
+        callback(['video1', 'video2', 'video3 ']);
+    },2000)
+}
 
-console.log(user);
+function videoDetails(video, callback){
+    setTimeout(() => {
+        callback('title of the video');
+    },2000)
+}
+
+
+const user = loginUser('devs@mail.com', 123456, user => {
+    console.log(user);
+    getUserVideo(user.userEmail, videos => {
+        console.log(videos);
+        videoDetails(video[0], title => {
+            console.log(title)
+        })
+    })
+});
+
+
+// console.log(user);
 
 console.log('Finish')
