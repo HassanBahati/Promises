5f792bab8b17e48700e68b0cd4988983a51fce74
HassanBahati <mukisabahati@gmail.com>
2021-03-24 15:53:02 +0300

    added promise

--- promise.js
+++ promise.js
@@ -1,1 +1,10 @@
-const promise = 
+const promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        console.log('ggghd')
+        resolve({user: 'bahati'})
+    },2000)
+});
+
+promise.then(user => {
+    console.log(user)
+})
