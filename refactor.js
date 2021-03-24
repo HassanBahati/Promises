d9b3cd8d2405da61137a62d0720072023b88cd37
HassanBahati <mukisabahati@gmail.com>
2021-03-24 18:08:56 +0300

    refactored refactor.js

---
+++ refactor.js
@@ -0,0 +1,51 @@
+console.log('Start');
+
+function loginUser(email, password){
+    return new Promise((reject, resolve) => {
+    setTimeout(()=>{
+        console.log('Now we have the data')
+        resolve({userEmail: email})
+    }, 5000);
+})}
+
+function getUserVideo(email){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(['video1', 'video2', 'video3 ']);
+        },2000)
+    })
+   
+}
+
+function videoDetails(video){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve('title of the video');
+        },2000)
+    })
+   
+}
+
+
+// const user = loginUser('devs@mail.com', 123456, user => {
+//     console.log(user);
+//     getUserVideo(user.userEmail, videos => {
+//         console.log(videos);
+//         videoDetails(videos[0], title => {
+//             console.log(title)
+//         })
+//     })
+// });
+
+loginUser('percy', 'gary')
+.then(user=> getUserVideo(user.email))
+.then(videos=> videoDetails(videos[0]))
+.then(detail=> console.log(detail))
+
+
+const user = loginUser('percy', 'gary')
+const videos = videoDetails(user.email)
+
+// console.log(user);
+
+console.log('Finish')
