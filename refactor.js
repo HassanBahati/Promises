164db941f7c6111afcc416257ad91eb233ed2769
HassanBahati <mukisabahati@gmail.com>
2021-03-24 18:18:08 +0300

    edited fb_yt.js

--- refactor.js
+++ refactor.js
@@ -51,21 +51,3 @@ function videoDetails(video){
 //sync
 
 // console.log('Finish')
-
-const yt = new Promise(resolve => {
-    setTimeout(()=> {
-        console.log('getting stuff from youtube');
-        resolve({videos: [1,2,3,4,5] });
-    },2000);
-}); 
-
-
-const fb = new Promise(resolve => {
-    setTimeout(()=> {
-        console.log('getting stuff from facebook');
-        resolve({videos: {user: 'Name'} });
-    },2000);
-}); 
-
-Promise.all([yt, fb])
-.then(result => console.log(result));
