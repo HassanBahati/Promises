f74344595ca37a73183f8074f243a7777d3df685
HassanBahati <mukisabahati@gmail.com>
2021-03-24 10:52:13 +0300

    edited  sync example

---
+++ async.js
@@ -0,0 +1,10 @@
+
+//async code example
+
+console.log('start');
+
+setTimeout(()=> {
+    console.log('We are in the time out');
+}, 2000);
+
+console.log('End');
--- sync.js
+++ sync.js
@@ -1,24 +1,14 @@
 //sync code example
 //execution in a specified format
 
-// function otherFunction(){
-//     console.log('We are in another function');
-//     console.log('do some stuff')
-// }
-
-// console.log('start');
-
-// otherFunction();
-
-// console.log('End');
-
-
-//async code example
+function otherFunction(){
+    console.log('We are in another function');
+    console.log('do some stuff')
+}
 
 console.log('start');
 
-setTimeout(()=> {
-    console.log('We are in the time out');
-}, 2000);
+otherFunction();
 
 console.log('End');
+
