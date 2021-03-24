c8ce68125a2c289bc93d6bde8863f4d0c1adc4f7
HassanBahati <mukisabahati@gmail.com>
2021-03-24 18:16:52 +0300

    fix bug

--- refactor.js
+++ refactor.js
@@ -37,15 +37,35 @@ function videoDetails(video){
 //     })
 // });
 
-loginUser('percy', 'gary')
-.then(user=> getUserVideo(user.email))
-.then(videos=> videoDetails(videos[0]))
-.then(detail=> console.log(detail))
+// loginUser('percy', 'gary')
+// .then(user=> getUserVideo(user.email))
+// .then(videos=> videoDetails(videos[0]))
+// .then(detail=> console.log(detail))
 
 
-const user = loginUser('percy', 'gary')
-const videos = videoDetails(user.email)
+// const user = loginUser('percy', 'gary')
+// const videos = videoDetails(user.email)
 
 // console.log(user);
 
-console.log('Finish')
+//sync
+
+// console.log('Finish')
+
+const yt = new Promise(resolve => {
+    setTimeout(()=> {
+        console.log('getting stuff from youtube');
+        resolve({videos: [1,2,3,4,5] });
+    },2000);
+}); 
+
+
+const fb = new Promise(resolve => {
+    setTimeout(()=> {
+        console.log('getting stuff from facebook');
+        resolve({videos: {user: 'Name'} });
+    },2000);
+}); 
+
+Promise.all([yt, fb])
+.then(result => console.log(result));
